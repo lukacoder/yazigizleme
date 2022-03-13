4c00fa3402b9d79dfa39b84f6255784f0d718761
Kedy Coder <101480197+kedyc0der@users.noreply.github.com>
2022-03-13 21:59:41 +0300

    Create stanog.py

---
+++ stanog.py
@@ -0,0 +1,17 @@
+import os
+
+print("""
+
+______________________________
+
+         Kedy.Coder
+
+______________________________
+
+""")
+
+veri = input("Private Text => ")
+
+veri2 = input("İmage File Name (example.jpg) => ")
+
+os.system("echo -n "+ veri + " >> "+veri2)
